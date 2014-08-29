62bd671451b6f91e132a5d22203c41f9886f09e6
prabackar <2raptor@gmail.com>
2014-08-28 23:55:38 -0700

    Update PermuationString.java

--- permutations/PermuationString.java
+++ permutations/PermuationString.java
@@ -7,19 +7,19 @@ public class PermuationString {
     permutation("", str); 
   }
 
-	private static void permutation(String prefix, String str) {
+  private static void permutation(String prefix, String str) {
     int n = str.length();
     if (n == 0) {
-    	System.out.println(prefix);
+      System.out.println(prefix);
     }
     else {
-        for (int i = 0; i < n; i++) {
-        		//System.out.println("i => " + i + " chatAt(i) "+str.charAt(i));
-						//System.out.println("perm IN("+prefix +"," +str+")"); 
-						//System.out.println("perm OUT("+prefix + str.charAt(i) + "," +  str.substring(0, i) + str.substring(i+1, n)+")");
-            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
-        }
+      for (int i = 0; i < n; i++) {
+        //System.out.println("i => " + i + " chatAt(i) "+str.charAt(i));
+        //System.out.println("perm IN("+prefix +"," +str+")"); 
+        //System.out.println("perm OUT("+prefix + str.charAt(i) + "," +  str.substring(0, i) + str.substring(i+1, n)+")");
+        permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+      }
     }
   }
 }
- 
+
