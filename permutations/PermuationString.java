8e01646db06fef8b0199d6bd0f810f01e582da91
prabackar <2raptor@gmail.com>
2014-08-28 23:35:57 -0700

    Update PermuationString.java

--- permutations/PermuationString.java
+++ permutations/PermuationString.java
@@ -1,16 +1,16 @@
 public class PermuationString {
   public static void main(String[] args) {
-		permutation("abc");
+    permutation("abc");
   }
 
   public static void permutation(String str) { 
     permutation("", str); 
-	}
+  }
 
 	private static void permutation(String prefix, String str) {
     int n = str.length();
     if (n == 0) {
-    	System.out.println("**"+prefix);
+    	System.out.println(prefix);
     }
     else {
         for (int i = 0; i < n; i++) {
@@ -20,6 +20,6 @@ public class PermuationString {
             permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
         }
     }
-}
+  }
 }
  
